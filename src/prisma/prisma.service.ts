c77b17acd6339a46d62a8b63f3807358668690df
KuraoHikari <dewaindra705@gmail.com>
2023-06-25 21:26:57 +0800

    update auth and prisma module

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const url = 'postgresql://postgres:123@localhost:5433/nestjs?schema=public';
+
+    super({
+      datasources: {
+        db: {
+          url,
+        },
+      },
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
